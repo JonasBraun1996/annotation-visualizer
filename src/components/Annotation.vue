9cbe2861b2049dd7518833f2c5262c838e61ab65
Jonas Braun <braunjonas1996googlemail.com>
2022-12-17 14:09:05 +0100

    chore(init): initial vue3 setup

---
+++ src/components/Annotation.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Annotation</h1>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script setup>
+
+</script>
+
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1>Annotation Visualizer</h1>
+    <h3>Prototype</h3>
+  </main>
+</template>
