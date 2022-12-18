46a6f7084f4f5162d76894f92c6246635c9f4ff0
Jonas Braun <braunjonas1996googlemail.com>
2022-12-18 21:36:07 +0100

    chore(structure): main component structure

---
+++ src/components/Navigation.vue
@@ -0,0 +1,31 @@
+<script>
+
+import Logo from './Logo.vue';
+
+export default {
+  name: "Navigation",
+  components: {
+    Logo
+  }
+};
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <logo class="d-lg-none"></logo>  
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <logo class="d-none d-lg-block"></logo>
+          <RouterLink class="nav-link" to="/">Home</RouterLink>
+          <RouterLink class="nav-link" to="/annotation">Annotation</RouterLink>
+        </div>
+      </div>
+    </div>
+  </nav>   
+</template>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../components/Home.vue'
+import Annotation from '../components/Annotation.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,12 +8,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home
     },
     {
       path: '/annotation',
       name: 'annotation',
-      component: () => import('../components/Annotation.vue')
+      component: Annotation
     }
   ]
 })
